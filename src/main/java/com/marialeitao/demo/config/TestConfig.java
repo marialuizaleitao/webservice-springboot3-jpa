2ac42d2f618d425ad750d68076f9ca23ef6eeb94
Maria Leitão <malufln.99@gmail.com>
2024-03-16 02:10:21 -0300

    Payment, one-to-one association

--- src/main/java/com/marialeitao/demo/config/TestConfig.java
+++ src/main/java/com/marialeitao/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.marialeitao.demo.entities.Category;
 import com.marialeitao.demo.entities.Order;
 import com.marialeitao.demo.entities.OrderItem;
+import com.marialeitao.demo.entities.Payment;
 import com.marialeitao.demo.entities.Product;
 import com.marialeitao.demo.entities.User;
 import com.marialeitao.demo.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 
 	}
 
